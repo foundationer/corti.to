19c37e9c39390b5771f658e3b795061224748834
Lucas Godoy <foundationer@tuta.io>
2018-06-24 21:17:56 -0300

    Added rudimentary list of links.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const client = new ApolloClient({
+    link: new HttpLink('https://api.graph.cool/simple/v1/cjitgitwv1vgm01892qi3gzez'),
+    cache: new InMemoryCache(),
+});
+
+const withApolloProvider = Comp => (
+  <ApolloProvider client={client}>{Comp}</ApolloProvider>
+);
+
+ReactDOM.render(
+    withApolloProvider(<App />),
+    document.getElementById('root'));
+
